555a253956529483509b24b290e7e1f8754c3d24
Joel Stein <js@funkturm.net>
2019-03-21 20:23:13 +0100

    add arm64 to abilist

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,7 +7,7 @@ TARGET_BOARD_PLATFORM := mt8173
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
+TARGET_CPU_ABI2 := arm64
 TARGET_CPU_VARIANT := generic
 
 TARGET_2ND_ARCH := arm
@@ -16,6 +16,8 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a7
 
+TARGET_CPU_ABI_LIST := arm64-v8a,arm64,armeabi-v7a,armeabi
+TARGET_CPU_ABI_LIST_64_BIT := arm64-v8a,arm64
 
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0.auto/gadget/lun0/file
 
@@ -27,25 +29,24 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x1000000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x1000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x1100000
 BOARD_HAS_NO_SELECT_BUTTON := true
 
-TW_THEME := portrait_hdpi
-
-#TARGET_RECOVERY_INITRC := $(DEVICE_PATH)/recovery/root/init.rc
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/recovery.fstab
-
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/zImage
-
-#BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00000000 --ramdisk_offset 0x03f80000 --second_offset 0x00e80000 --tags_offset 0x07f80000
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00000000 --ramdisk_offset 0x03400000 --second_offset 0x00e80000 --tags_offset 0x07f80000
 # original - not enough space for us!
 # BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00000000 --ramdisk_offset 0x03f80000 --second_offset 0x00e80000 --tags_offset 0x07f80000
 
-#TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone2/temp
+TARGET_USERIMAGES_USE_EXT4 := true
+
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/zImage
+
+#TARGET_RECOVERY_INITRC := $(DEVICE_PATH)/recovery/root/init.rc
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/recovery.fstab
+TARGET_RECOVERY_PIXEL_FORMAT := ABGR_8888
 
 RECOVERY_SDCARD_ON_DATA := true
-TW_HACKED_BL_BUTTON := true
 
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_RECOVERY_PIXEL_FORMAT := ABGR_8888
+TW_THEME := portrait_hdpi
+TW_HACKED_BL_BUTTON := true
+#TW_INCLUDE_L_CRYPTO := true
+#TARGET_HW_DISK_ENCRYPTION := true
