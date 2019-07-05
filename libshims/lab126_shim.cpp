b9e4ac4e1b73644d5812c57261d33030ad989cc5
Joel Stein <js@funkturm.net>
2019-07-05 21:50:52 +0200

    update lab126_shim to redirect logging to logcat

--- libshims/lab126_shim.cpp
+++ libshims/lab126_shim.cpp
@@ -2,11 +2,9 @@
 #include <log/log.h>
 //extern "C" void lab126_log_write(void) {}
 
-extern "C" int lab126_log_write(int loglevel, uint32_t * b, const char * logtag, const char * fmt, ...) {
+extern "C" int lab126_log_write(int loglevel, uint32_t * unknown, const char * logtag, const char * fmt, ...) {
 	va_list args;
 	va_start(args, fmt);
-	//printf("%s: ", logtag, b);
-	//vprintf(fmt, args);
 	LOG_PRI_VA(loglevel, logtag, fmt, args);
 	va_end(args);
 	return 0;
